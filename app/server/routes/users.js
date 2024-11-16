b44c0da53a15cce7998a7c5ce85f04fdede0c492
Imad <naitmihoubimzd@gmail.com>
2024-11-16 01:19:01 +0100

    Resolving conflicts

--- app/server/routes/users.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var userHandler_1 = require("../handlers/userHandler");
-var router = (0, express_1.Router)();
-router.get('/getUsers', userHandler_1.getUsers);
-router.post('/new', userHandler_1.createUser);
-exports.default = router;
